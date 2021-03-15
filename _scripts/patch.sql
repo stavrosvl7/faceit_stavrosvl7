deee4a1365a2654564a3c41dc1c09bf7f2730b58
stavrosvl7 <stavrosvl7@gmail.com>
2021-03-16 01:33:08 +0200

    improvements

--- _scripts/patch.sql
+++ _scripts/patch.sql
@@ -16,13 +16,12 @@ CREATE TABLE web_user (
     nick_name text ,
     password text not null,
     email text unique not null,
-    byte_payload text,
     country_id bigint not null,
     CONSTRAINT country_id_fk FOREIGN KEY (country_id) REFERENCES dom_country (id)
 );
 
 insert into dom_country values(nextval('dom_country_id_seq'),'Greece','gr','+30');
 insert into dom_country values(nextval('dom_country_id_seq'),'USA','us','+1');
-insert into web_user values(nextval('web_user_id_seq'),'gianna','rouvali','ioannarouv15','$2y$10$K/KG1GjUw9TCp3vAltw3/OEpBcuVWpSzbIigI/WmGDAE6Brab0lz2 ','ioannarouv15@gmail.com',null,'1');
-insert into web_user values(nextval('web_user_id_seq'),'kostas','rouvalis','zigkompikoule','$2y$10$K/KG1GjUw9TCp3vAltw3/OEpBcuVWpSzbIigI/WmGDAE6Brab0lz2 ','zigkompikoule@gmail.com',null,'2');
-insert into web_user values(nextval('web_user_id_seq'),'stavros','vlachakis','stavrosvl7','$2y$10$K/KG1GjUw9TCp3vAltw3/OEpBcuVWpSzbIigI/WmGDAE6Brab0lz2 ','stavrosvl7@gmail.com',null,'1');
+insert into web_user values(nextval('web_user_id_seq'),'gianna','rouvali','ioannarouv15','$2y$10$K/KG1GjUw9TCp3vAltw3/OEpBcuVWpSzbIigI/WmGDAE6Brab0lz2 ','ioannarouv15@gmail.com','1');
+insert into web_user values(nextval('web_user_id_seq'),'kostas','rouvalis','zigkompikoule','$2y$10$K/KG1GjUw9TCp3vAltw3/OEpBcuVWpSzbIigI/WmGDAE6Brab0lz2 ','zigkompikoule@gmail.com','2');
+insert into web_user values(nextval('web_user_id_seq'),'stavros','vlachakis','stavrosvl7','$2y$10$K/KG1GjUw9TCp3vAltw3/OEpBcuVWpSzbIigI/WmGDAE6Brab0lz2 ','stavrosvl7@gmail.com','1');
